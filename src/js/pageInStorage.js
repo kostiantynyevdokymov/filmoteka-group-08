0a6e6c4d490d0aa9de5aae4aa6d59a39759de840
Evgeniy Frolov <95540052+EvgeniyTIR@users.noreply.github.com>
2022-07-19 20:24:18 +0300

    fix currentpage

--- src/js/pageInStorage.js
+++ src/js/pageInStorage.js
@@ -1,14 +1,15 @@
 import storage from './storage';
-import { currentPage } from './pagination';
-
+// import { currentPage } from './pagination';
+import * as page from './pagination';
+console.log(page.currentPage)
 export const STORAGE_PAGE_KEY = 'last visit page';
 export const storagePage = storage.load(STORAGE_PAGE_KEY);
 
 changeStoragePage();
 
 function changeStoragePage() {
-  if (currentPage > 1 && currentPage !== storagePage?.value) {
+  if (page.currentPage > 1 && page.currentPage !== storagePage?.value) {
     storage.remove(STORAGE_PAGE_KEY);
-    storage.save(STORAGE_PAGE_KEY, { value: currentPage });
+    storage.save(STORAGE_PAGE_KEY, { value: page.currentPage });
   }
 }
