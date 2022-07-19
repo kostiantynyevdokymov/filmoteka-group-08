9249d728b41b3719d4ce139270457269982e195b
Vera957 <good.answer.e@gmail.com>
2022-07-19 23:33:15 +0300

    css, btn-active, html classes

--- src/js/libFunc.js
+++ src/js/libFunc.js
@@ -3,17 +3,10 @@ import { movieCards } from "./fetchMovies";
 const libraryContainer = document.querySelector('.library-container');
 export const libList = document.querySelector('.library-list');
 
-const active_type = 'header__btn-active';
+export const active_type = 'header__btn-active';
 
 export function funnyGuyOnBg() {
-    const funny = libraryContainer.classList.contains('lib-bg-img');
-    if (!libList.firstChild && !funny) {
-        libraryContainer.classList.add('lib-bg-img');
-    } else if (!libList.firstChild && funny) {
-        return;
-    } else {
-        libraryContainer.classList.remove('lib-bg-img');
-    }
+    libList.hasChildNodes ? libraryContainer.classList.remove('lib-bg-img ') : libraryContainer.classList.add('lib-bg-img');
 }
 
 export function renderCardsFromLocalStoradge(key, ul) {
@@ -23,14 +16,12 @@ export function renderCardsFromLocalStoradge(key, ul) {
         alert('Your list is empty, please fill it');
         return;
     };
+    console.log('renderCardsFromLocalStoradge')
     const parsedDataLib = JSON.parse(dataLib);
     ul.innerHTML = movieCards(parsedDataLib);
-}
-
-export function checkBtns(btn) {
-    if (!btn.classList.contains(active_type)) btn.classList.add(active_type);
 }
 
 export function removeMarkUp(elem) {
     elem.innerHTML = '';
 }
+
