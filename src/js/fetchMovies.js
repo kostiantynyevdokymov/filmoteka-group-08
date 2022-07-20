83bfb10e5aa974e2c37ab2086acb64874b447d4c
Mykhailo Shyhin <johnglagol@gmail.com>
2022-07-20 17:45:40 +0300

    fix build bug

--- src/js/fetchMovies.js
+++ src/js/fetchMovies.js
@@ -27,7 +27,7 @@ formField?.addEventListener('submit', event => {
   });
 });
 
-function movieCards(movies) {
+export function movieCards(movies) {
   return movies
     .map(({ id, poster_path, title, original_title, genres_ids, release_date }) => {
       const imgUrl = poster_path
