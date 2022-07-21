4fc6860ddb9f7fe090d563a9bbacb347c20abb48
Mykhailo Shyhin <johnglagol@gmail.com>
2022-07-21 16:04:57 +0300

    add js

---
+++ src/js/movieCard.js
@@ -0,0 +1,24 @@
+export function movieCards(movies) {
+  return movies
+    .map(({ id, poster_path, title, original_title, genres_ids, release_date }) => {
+      const imgUrl = poster_path
+        ? `https://image.tmdb.org/t/p/w500${poster_path}`
+        : // : './images/netuNichego.png';
+          'https://via.placeholder.com/395x574/FFFFFF/FF001B?text=No+poster';
+      const year = new Date(release_date).getFullYear();
+      return `<li class="home-card js-modal-open placeholdify" data-card-movie-id="${id}">
+            <a href="#" class="home-card__link">
+                <div class="card-info">
+                    <img class="home-card__img" src="${imgUrl}" alt="${title}">
+                    <h2 class="card-info__title">${original_title}</h2>
+                    <p class="card-info_descr">
+                        <span>${genres_ids}</span>
+                        |
+                        <span>${year}</span>
+                    </p>
+                </div>
+            </a>
+        </li>`;
+    })
+    .join('');
+};
