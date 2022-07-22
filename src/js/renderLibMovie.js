08f0f168a5670aa5198786878fdb8445a345f388
Viktoriia Hrytsenko <dev.viktoriia.h@gmail.com>
2022-07-22 22:29:57 +0300

    fix undefined error in home page

--- src/js/renderLibMovie.js
+++ src/js/renderLibMovie.js
@@ -1,16 +1,55 @@
 import { markUpWithGenres } from './cardListWithGenres';
 import { removeSceletonLoad } from './sceletonLoad';
 
-//containers for render, design changes
-const libraryContainer = document.querySelector('.library-container');
-const libList = document.querySelector('.library-list');
-const spinner = document.querySelector('.spinner-loader');
-//btn accent
-const active_type = 'header__btn-active';
+if (document.querySelector('.library-container')) {
+  //containers for render, design changes
+  const libList = document.querySelector('.library-list');
+  const spinner = document.querySelector('.spinner-loader');
+  //btn accent
+  const active_type = 'header__btn-active';
 
-//listerners
-const btnOpenWatched = document.querySelector('.js-watched-btn');
-const btnOpenQue = document.querySelector('.js-queue-btn');
+  //listerners
+  const btnOpenWatched = document.querySelector('.js-watched-btn');
+  const btnOpenQue = document.querySelector('.js-queue-btn');
+
+  window.onload = () => {
+    libList.innerHTML = markUpWithGenres(arrValuesOfQueue);
+    funnyGuyOnBg();
+    spinner.classList.remove('is-hidden');
+    setTimeout(() => {
+      spinner.classList.add('is-hidden');
+    }, 2000);
+    removeSceletonLoad();
+  };
+
+  //queue btn is active on load
+  btnOpenQue?.addEventListener('click', openQueue);
+  btnOpenWatched?.addEventListener('click', openWatched);
+
+  function openQueue() {
+    libList.innerHTML = markUpWithGenres(arrValuesOfQueue);
+    funnyGuyOnBg();
+    spinner.classList.remove('is-hidden');
+    setTimeout(() => {
+      spinner.classList.add('is-hidden');
+    }, 2000);
+    removeSceletonLoad();
+    btnOpenQue.classList.add(active_type);
+    btnOpenWatched.classList.remove(active_type);
+  }
+
+  function openWatched() {
+    libList.innerHTML = markUpWithGenres(arrValuesOfWatched);
+    funnyGuyOnBg();
+    spinner.classList.remove('is-hidden');
+    setTimeout(() => {
+      spinner.classList.add('is-hidden');
+    }, 2000);
+    removeSceletonLoad();
+    btnOpenQue.classList.remove(active_type);
+    btnOpenWatched.classList.add(active_type);
+  }
+}
 
 //data parsed
 export const queueStorage = localStorage.getItem('queue-list');
@@ -20,45 +59,10 @@ export const arrValuesOfWatched = JSON.parse(watchedStorage);
 export const valuesOfGenres = localStorage.getItem('arrow');
 export const arrValuesOfGenres = JSON.parse(valuesOfGenres);
 
-window.onload = () => {
-    libList.innerHTML = markUpWithGenres(arrValuesOfQueue);
-    funnyGuyOnBg();
-      spinner.classList.remove('is-hidden');
-    setTimeout(() => { spinner.classList.add('is-hidden') }, 2000);
-    removeSceletonLoad();
-}
-
-//queue btn is active on load
-btnOpenQue?.addEventListener('click', openQueue);
-btnOpenWatched?.addEventListener('click', openWatched);
-
-
-function openQueue() {
-    libList.innerHTML = markUpWithGenres(arrValuesOfQueue);
-    funnyGuyOnBg();
-    spinner.classList.remove('is-hidden');
-    setTimeout(() => { spinner.classList.add('is-hidden') }, 2000);
-    removeSceletonLoad();
-    btnOpenQue.classList.add(active_type);
-    btnOpenWatched.classList.remove(active_type);
-}
-
-function openWatched() {
-    libList.innerHTML = markUpWithGenres(arrValuesOfWatched);
-    funnyGuyOnBg();
-     spinner.classList.remove('is-hidden');
-    setTimeout(() => { spinner.classList.add('is-hidden') }, 2000);
-    removeSceletonLoad();
-    btnOpenQue.classList.remove(active_type);
-    btnOpenWatched.classList.add(active_type);
-}
-
 export function funnyGuyOnBg() {
-    libList.firstChild == null ? libraryContainer.classList.add('lib-bg-img') : libraryContainer.classList.remove('lib-bg-img');
+  const libraryContainer = document.querySelector('.library-container');
+
+  libList.firstChild == null
+    ? libraryContainer.classList.add('lib-bg-img')
+    : libraryContainer.classList.remove('lib-bg-img');
 }
-
-
-
-
-
-
