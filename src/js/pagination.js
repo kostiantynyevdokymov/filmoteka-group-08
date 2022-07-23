c792fa3b9bf3800b0bb547e19c63c2b764257d26
Evgeniy Frolov <95540052+EvgeniyTIR@users.noreply.github.com>
2022-07-23 12:21:02 +0300

    header-center-elements

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -133,4 +133,18 @@ function defineResultsPerPage() {
 
 
 export { currentPage, defineResultsPerPage };
+  
+export const backOnFirstBtn = () => { 
+   btns.forEach(el => el.classList.remove('pagination--current'));
+      btn1Ref.textContent = 1;
+      btn2Ref.textContent = 2;
+      btn3Ref.textContent = 3;
+      btn4Ref.textContent = 4;
+      btn5Ref.textContent = 5;
+      btn1Ref.classList.add('pagination--current');
+      currentPage = btn1Ref.textContent;
+      leftArrowRef.hidden = true;
+      prevDotsRef.hidden = true;
+      firstPageRef.hidden = true;
+}
 
