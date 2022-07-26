e43f8484112196895f3b30f0be62f2ccc260564f
Kostiantyn <cashalot80@ukr.net>
2022-07-26 13:54:00 +0100

    theme switch

---
+++ src/js/theme-switch.js
@@ -0,0 +1,36 @@
+
+const checkboxEl = document.querySelector('.theme-switch__toggle');
+const modalEl = document.querySelector('.modal');
+const studentsModalEl = document.querySelector('.modal');
+const modalCloseEl = document.querySelector('.close-icon');
+const bodyEl = document.querySelector('body');
+
+
+// переключение темы
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+bodyEl.classList.add(Theme.LIGHT);
+
+if (localStorage.getItem('theme')) {
+    checkboxEl.setAttribute('checked', true);
+    bodyEl.classList.add(Theme.DARK);
+    modalEl.classList.add(Theme.DARK);
+    studentsModalEl.classList.add(Theme.DARK);
+    modalCloseEl.classList.add(Theme.DARK);
+}
+
+const onChange = () => {
+    bodyEl.classList.toggle(Theme.DARK);
+    modalEl.classList.toggle(Theme.DARK);
+    studentsModalEl.classList.toggle(Theme.DARK);
+    modalCloseEl.classList.toggle(Theme.DARK);
+
+    if (bodyEl.classList.contains(Theme.DARK)) {
+        localStorage.setItem('theme', 'dark');
+    } else {
+        localStorage.removeItem('theme');
+    }    
+};
+checkboxEl.addEventListener('change', onChange);
